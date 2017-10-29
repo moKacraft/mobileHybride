808ddaeae5c89566b2279514fb0325a7272d1968
Unknown <nicolaspinatel@gmail.com>
2017-10-29 18:51:38 +0100

    Geolocalisation working !
    Geolocalisation working !

--- App.js
+++ App.js
@@ -5,11 +5,13 @@
  */
 
 import React, { Component } from 'react';
+import MapView from 'react-native-maps';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -19,19 +21,72 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+let { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 0;
+const LONGITUDE = 0;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
 export default class App extends Component<{}> {
+  constructor() {
+    super();
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
+    };
+  }
+  componentDidMount() {
+    console.log("hello");
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      this.setState({
+        region: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }
+      });
+    },
+  (error) => console.log(error.message),
+  { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+  );
+  this.watchID = navigator.geolocation.watchPosition(
+    position => {
+      this.setState({
+        region: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }
+      });
+    }
+  );
+}
+componentWillUnmount() {
+  navigator.geolocation.clearWatch(this.watchID);
+}
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <MapView
+        style={styles.map}
+        showsUserLocation={ true }
+        region={ this.state.region }
+        onRegionChange={ region => this.setState({region}) }
+        onRegionChangeComplete={ region => this.setState({region}) }
+      >
+        <MapView.Marker
+          coordinate={ this.state.region }
+        />
+      </MapView>
       </View>
     );
   }
@@ -43,6 +98,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  map: {
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: 0,
+    position: 'absolute'
   },
   welcome: {
     fontSize: 20,
