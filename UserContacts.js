59ed883ad40d48a73a2c5059daf480cc3838d079
moKacraft <mokacraft@gmail.com>
2017-10-29 23:13:13 +0100

    warning list contact
    encore des modifs a faire

--- UserContacts.js
+++ UserContacts.js
@@ -7,31 +7,66 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 import Contacts from "react-native-contacts";
+import { List, ListItem, SearchBar } from "react-native-elements";
 
 export default class UserContacts extends Component<{}> {
   constructor() {
     super();
     this.state = {
       textValue:'Click the button to display contacts',
-      data: []
+      contact: []
     }
   }
   getContacts() {
     Contacts.getAll((err, data) => {
       if (err)
       throw err;
+      else {
+        this.setState({
+          contact: data
+        });
+      }
 
       for (let item of data) {
         console.log('getAll:', item)
         this.setState({
-          textValue: item.givenName,
-          data: item
+          textValue: item.givenName
         });
       }
     })
   }
 
 
+  renderSeparator = () => {
+  return (
+    <View
+      style={{
+        height: 1,
+        width: "86%",
+        backgroundColor: "#CED0CE",
+        marginLeft: "14%"
+      }}
+    />
+  );
+};
+
+renderHeader = () => {
+  return <SearchBar placeholder="Type Here..." lightTheme round />;
+};
+
+renderFooter = () => {
+
+  return (
+    <View
+      style={{
+        paddingVertical: 20,
+        borderTopWidth: 1,
+        borderColor: "#CED0CE"
+      }}
+    >
+    </View>
+  );
+};
 
   render() {
     return (
@@ -42,13 +77,23 @@ export default class UserContacts extends Component<{}> {
       color="#841584"/>
       <Text style={styles.hello}>{this.state.textValue}</Text>
 
-      <FlatList
-      data={this.state.data}
+      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+        <FlatList
+          data={this.state.contact}
           renderItem={({ item }) => (
-            <Text style={styles.hello}>{this.state.textValue}</Text>
+            <ListItem
+              roundAvatar
+              title={`${this.state.textValue} ${this.state.contact.givenName}`}
+              subtitle={this.state.textValue}
+              containerStyle={{ borderBottomWidth: 0 }}
+            />
           )}
-          keyExtractor={item => this.state.data.email}
-          />
+          keyExtractor={item => item.email}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+          ListFooterComponent={this.renderFooter}
+        />
+      </List>
       </View>
     );
   }
--- android/app/src/main/java/com/mobilehybride/MainApplication.java
+++ android/app/src/main/java/com/mobilehybride/MainApplication.java
@@ -3,6 +3,7 @@ package com.mobilehybride;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new ReactNativeContacts(),
             new MapsPackage()
       );
