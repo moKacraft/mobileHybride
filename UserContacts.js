3110b1f0bd32e34d35977344c5972dd0c4b5abf9
moKacraft <mokacraft@gmail.com>
2017-10-30 22:12:59 +0100

    contacts pictures

--- UserContacts.js
+++ UserContacts.js
@@ -6,17 +6,19 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
-import Contacts from "react-native-contacts";
-import { List, ListItem, SearchBar } from "react-native-elements";
+import Contacts from 'react-native-contacts';
+import { List, ListItem, SearchBar } from 'react-native-elements';
 
 export default class UserContacts extends Component<{}> {
   constructor() {
     super();
     this.state = {
       textValue:'Click the button to display contacts',
-      contact: []
+      contact: [],
+      default: '/assets/default.jpg',
     }
   }
+
   getContacts() {
     Contacts.getAll((err, data) => {
       if (err)
@@ -26,73 +28,60 @@ export default class UserContacts extends Component<{}> {
           contact: data
         });
       }
-
-      for (let item of data) {
-        console.log('getAll:', item)
-        this.setState({
-          textValue: item.givenName
-        });
-      }
     })
   }
 
-
   renderSeparator = () => {
-  return (
-    <View
+    return (
+      <View
       style={{
         height: 1,
-        width: "86%",
-        backgroundColor: "#CED0CE",
-        marginLeft: "14%"
+        width: '100%',
+        backgroundColor: '#CED0CE'
       }}
-    />
-  );
-};
+      />
+    );
+  };
 
-renderHeader = () => {
-  return <SearchBar placeholder="Type Here..." lightTheme round />;
-};
+  renderHeader = () => {
+    return <SearchBar placeholder="Type Here..." lightTheme round />;
+  };
 
-renderFooter = () => {
-
-  return (
-    <View
+  renderFooter = () => {
+    return (
+      <View
       style={{
         paddingVertical: 20,
         borderTopWidth: 1,
-        borderColor: "#CED0CE"
+        borderColor: '#CED0CE'
       }}
-    >
-    </View>
-  );
-};
+      >
+      </View>
+    );
+  };
 
   render() {
     return (
       <View>
       <Button
       onPress={this.getContacts.bind(this)}
-      title="Get contacts"
-      color="#841584"/>
-      <Text style={styles.hello}>{this.state.textValue}</Text>
-
+      title='Get contacts'
+      color='#841584'/>
       <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
-        <FlatList
-          data={this.state.contact}
-          renderItem={({ item }) => (
-            <ListItem
-              roundAvatar
-              title={`${this.state.textValue} ${this.state.contact.givenName}`}
-              subtitle={this.state.textValue}
-              containerStyle={{ borderBottomWidth: 0 }}
-            />
-          )}
-          keyExtractor={item => item.email}
-          ItemSeparatorComponent={this.renderSeparator}
-          ListHeaderComponent={this.renderHeader}
-          ListFooterComponent={this.renderFooter}
+      <FlatList
+      data={this.state.contact}
+      renderItem={({ item }) => (
+        <ListItem
+        roundAvatar
+        title={`${item.givenName}`}
+        subtitle={`${item.phoneNumbers[0].label} ${item.phoneNumbers[0].number}`}
+        avatar={ item.hasThumbnail ? {uri: item.thumbnailPath} : require('./assets/default.jpg') }
+        containerStyle={{ borderBottomWidth: 0 }}
         />
+      )}
+      keyExtractor={item => item.givenName}
+      ItemSeparatorComponent={this.renderSeparator}
+      />
       </List>
       </View>
     );
