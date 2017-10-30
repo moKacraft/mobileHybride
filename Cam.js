c8c78346d9e59f5bd013c7492c8942b9a728ae06
moKacraft <mokacraft@gmail.com>
2017-10-30 23:09:48 +0100

    camera switch front and back

--- Cam.js
+++ Cam.js
@@ -10,46 +10,80 @@ import {
 import Camera from 'react-native-camera';
 
 export default class Cam extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    camera: {
+      aspect: Camera.constants.Aspect.fill,
+      captureTarget: Camera.constants.CaptureTarget.cameraRoll,
+      type: Camera.constants.Type.front,
+      orientation: Camera.constants.Orientation.auto,
+      flashMode: Camera.constants.FlashMode.auto,
+    }
+  };
+}
   render() {
-      return (
-        <View style={styles.container}>
-          <Camera
-            ref={(cam) => {
-              this.camera = cam;
-            }}
-            style={styles.preview}
-            aspect={Camera.constants.Aspect.fill}>
-            <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
-          </Camera>
-        </View>
-      );
-    }
-
-    takePicture() {
-      const options = {};
-      //options.location = ...
-      this.camera.capture({metadata: options})
-        .then((data) => console.log(data))
-        .catch(err => console.error(err));
-    }
+    return (
+      <View style={styles.container}>
+      <Camera
+      ref={(cam) => {
+        this.camera = cam;
+      }}
+      style={styles.preview}
+      orientation={Camera.constants.Orientation.auto}
+      type={this.state.camera.type}
+      aspect={Camera.constants.Aspect.fill}>
+      <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+      <Text style={styles.capture} onPress={this.switchType.bind(this)}>[SWITCH]</Text>
+      </Camera>
+      </View>
+    );
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'row',
-    },
-    preview: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      alignItems: 'center'
-    },
-    capture: {
-      flex: 0,
-      backgroundColor: '#fff',
-      borderRadius: 5,
-      color: '#000',
-      padding: 10,
-      margin: 40
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+    .then((data) => console.log(data))
+    .catch(err => console.error(err));
+  }
+
+  switchType() {
+    let newType;
+    const { back, front } = Camera.constants.Type;
+
+    if (this.state.camera.type === back) {
+      newType = front;
+    } else if (this.state.camera.type === front) {
+      newType = back;
     }
-  });
+
+    this.setState({
+      camera: {
+        ...this.state.camera,
+        type: newType,
+      },
+    });
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 5,
+    margin: 20
+  }
+});
